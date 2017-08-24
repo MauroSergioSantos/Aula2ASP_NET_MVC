e1d7bb37855a7ab00d73fd4c6e260f9a290e3c9b
MAUROSERGIO\Chrystian <mauro_mauro1969@hotmail.com>
2017-08-23 22:24:12 -0300

    Add project files.

---
+++ WebApp_Aula2Lab2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApp_Aula2Lab2.MvcApplication" Language="C#" %>
---
+++ WebApp_Aula2Lab2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp_Aula2Lab2.Startup))]
+namespace WebApp_Aula2Lab2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
